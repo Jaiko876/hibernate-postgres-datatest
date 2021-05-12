c045362036442fdb227c56b856480b727ca5e628
Молоствов Игорь <Jaiko876@gmail.com>
2021-05-12 19:48:30 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernatepostgresdatatest/HibernatePostgresDatatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernatepostgresdatatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePostgresDatatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernatePostgresDatatestApplication.class, args);
+    }
+
+}
